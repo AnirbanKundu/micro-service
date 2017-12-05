23b730748d8c7dd2f7b1585e9b4717488e287859
a.kundu@ge.com <a.kundu@ge.com>
2017-12-05 07:43:29 -0800

    Adding the STUF Call

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
 var express = require('express'),
 bodyParser = require('body-parser');
-
-
+const request = require('request-promise');
 var app = express();
 app.use(bodyParser.json());
-
-
-
 
 app.get('/get', function (req, res, next) {
     console.log('The token is :',req.header('Authorization'));
     var token = req.header('Authorization') || 'NO TOKEN';
-    let data = ['IceCream', 'Chocolates','Candy','Juice']
-    res.json({'auth':token,'data':data});
+    let data = ['IceCream', 'Chocolates','Candy','Juice'];
+    var tenant = req.header('tenant') || ' ';
+    if(token && tenant){
+        const options = {
+            url: 'https://users-stuf-stufrc.apm.aws-usw02-pr.predix.io/v1/users/me/checkaccess',
+            headers: {
+              'cache-control': 'no-cache',
+              'content-type': 'application/json',
+              Authorization: token,
+              tenant: tenant
+            },
+            json: true,
+            body: {
+              'uri':req.url,
+              'httpMethod':'POST'
+            }
+        };
+        request.post(options, (err, resp, data) => {
+            const statusCode = (resp) ? resp.statusCode : 502;
+            if(err || statusCode !== 200) {
+              err = err || `Error getting verdict: ${statusCode}`;
+              res.json({'auth':'Error !!!'});
+            } else {
+                res.json({'auth':'Pass'});
+            }
+        });
+    }    
+    else{
+        res.json({'auth':token,'data':data,'message':'No token and Tenant present in header'});
+    }    
 });
 
 var PORT =  process.env.PORT || 9002;
