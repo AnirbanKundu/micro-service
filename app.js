2e6a9390aa865011c3e39f8e87d72c3a1a60f340
a.kundu@ge.com <a.kundu@ge.com>
2017-12-05 12:19:39 -0800

    Removing the stuf call

--- app.js
+++ app.js
@@ -9,34 +9,35 @@ app.get('/get', function (req, res, next) {
     var token = req.header('Authorization') || 'NO TOKEN';
     let data = ['IceCream', 'Chocolates','Candy','Juice'];
     var tenant = req.header('tenant') || ' ';
-    if(token && tenant){
-        const options = {
-            url: 'https://users-stuf-stufrc.apm.aws-usw02-pr.predix.io/v1/users/me/checkaccess',
-            headers: {
-              'cache-control': 'no-cache',
-              'content-type': 'application/json',
-              Authorization: token,
-              tenant: tenant
-            },
-            json: true,
-            body: {
-              'uri':req.url,
-              'httpMethod':'POST'
-            }
-        };
-        request.post(options, (err, resp, data) => {
-            const statusCode = (resp) ? resp.statusCode : 502;
-            if(err || statusCode !== 200) {
-              err = err || `Error getting verdict: ${statusCode}`;
-              res.json({'auth':'Error !!!'});
-            } else {
-                res.json({'auth':'Pass'});
-            }
-        });
-    }    
-    else{
-        res.json({'auth':token,'data':data,'message':'No token and Tenant present in header'});
-    }    
+    // if(token && tenant){
+    //     const options = {
+    //         url: 'https://users-stuf-stufrc.apm.aws-usw02-pr.predix.io/v1/users/me/checkaccess',
+    //         headers: {
+    //           'cache-control': 'no-cache',
+    //           'content-type': 'application/json',
+    //           Authorization: token,
+    //           tenant: tenant
+    //         },
+    //         json: true,
+    //         body: {
+    //           'uri':req.url,
+    //           'httpMethod':'POST'
+    //         }
+    //     };
+    //     request.post(options, (err, resp, data) => {
+    //         const statusCode = (resp) ? resp.statusCode : 502;
+    //         if(err || statusCode !== 200) {
+    //           err = err || `Error getting verdict: ${statusCode}`;
+    //           res.json({'auth':'Error !!!'});
+    //         } else {
+    //             res.json({'auth':'Pass'});
+    //         }
+    //     });
+    // }    
+    // else{
+    //     res.json({'auth':token,'data':data,'message':'No token and Tenant present in header'});
+    // }   
+    res.json({'auth':token,'data':data,'message':'No token and Tenant present in header'}); 
 });
 
 var PORT =  process.env.PORT || 9002;
